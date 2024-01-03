b6e508c6c3eb62fc62fafd960e45713a39ec3a56
lss018000 <lss018000@gmail.com>
2024-01-04 06:26:54 +0900

    20240104-01

--- oz_project/Admin페이지만들기/product_get.py
+++ oz_project/Admin페이지만들기/product_get.py
@@ -1,7 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pandas
-resp = requests.get('')
+import pandas as pd
+
+resp = requests.get('https://kream.co.kr/search?tab=43')
 html = resp.text
 soup = BeautifulSoup(html, 'html.parser')
-news = soup.select('.search_result')
+products = soup.select('.search_result_list div')
+
+cat = [] # Category
+brand = [] #brand
+title = [] #title
+price = [] #price
+
+for n in products:
+    cat.append(n.text)
+    brand.append(n['href'])
+    
+df = pd.DataFrame()
+df['제목'] = cat
+df['url'] = brand
+
+df.to_excel('test.xlsx', index = False);
