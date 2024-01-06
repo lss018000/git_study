2c461d3ce64fbaf4e9759b3e06e1fda293cdf213
lss018000 <lss018000@gmail.com>
2024-01-07 05:37:48 +0900

    20240107_1

--- oz_project/Admin페이지만들기/product_get.py
+++ oz_project/Admin페이지만들기/product_get.py
@@ -1,23 +1,71 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
-resp = requests.get('https://kream.co.kr/search?tab=43')
-html = resp.text
-soup = BeautifulSoup(html, 'html.parser')
-products = soup.select('.search_result_list div')
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-cat = [] # Category
-brand = [] #brand
-title = [] #title
-price = [] #price
+data_types = [
+    ['https://kream.co.kr/search?gender=&shop_category_id=64', '64', ''], #상의
+    ['https://kream.co.kr/search?gender=men&shop_category_id=64', '64', 'men'], #상의 남자
+    ['https://kream.co.kr/search?gender=women&shop_category_id=64', '64', 'women'], #상의 남자
+    ['https://kream.co.kr/search?gender=&shop_category_id=65', '65', ''], #하의
+    ['https://kream.co.kr/search?gender=men&shop_category_id=65', '65', 'men'], #하의 남자
+    ['https://kream.co.kr/search?gender=women&shop_category_id=65', '65', 'women'], #하의 여자
+    ['https://kream.co.kr/search?gender=&shop_category_id=34', '34', ''], #신발
+    ['https://kream.co.kr/search?gender=men&shop_category_id=34', '34', 'men'], #신발 남자
+    ['https://kream.co.kr/search?gender=women&shop_category_id=34', '34', 'women'], #신발 여자
+    ['https://kream.co.kr/search?gender=&shop_category_id=7', '7', ''], #패션잡화
+    ['https://kream.co.kr/search?gender=men&shop_category_id=7', '7', 'men'], #패션잡화 남자
+    ['https://kream.co.kr/search?gender=women&shop_category_id=7', '7', 'women'] #패션잡화 여자
+]
 
-for n in products:
-    cat.append(n.text)
-    brand.append(n['href'])
-    
-df = pd.DataFrame()
-df['제목'] = cat
-df['url'] = brand
+for data_type in data_types:
+    search_url = data_type[0];
+    print(search_url);
+    driver.get(search_url)
+    # driver.get("https://kream.co.kr/search?tab=43")
+    data_name ='kream_data_'+data_type[1]+'_'+data_type[2];
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    products = soup.select('div.search_result_list > div.product_card')
+    array_row = []
+    for product in products:
+        url = product.find('a')['href'];
+        brand = product.select_one('a > .product_info_area > .title > .brand').text
+        name = product.select_one('a > .product_info_area > .title > .product_info_product_name > .translated_name').text
+        price = product.select_one('a > .price_area > .amount').text
+        if data_type[1] == '64':
+            cat_name = '상의';
+        elif data_type[1] == '65':
+            cat_name = '하의';
+        elif data_type[1] == '34':
+            cat_name = '신발';
+        elif data_type[1] == '7':
+            cat_name = '패션잡화';
+            
+        if data_type[2] == 'men':
+            sex_val = '남자';
+        elif data_type[2] == 'women':
+            sex_val = '여자';   
+        else:
+            sex_val = '남자/여자';         
+            
 
-df.to_excel('test.xlsx', index = False);
+        
+        row = {
+            'category': cat_name,
+            'brand': brand,
+            'sex': sex_val,
+            'product': name,
+            'price': price,
+            'url': url,
+        }
+        array_row.append(row) 
+    file_path = './oz_project/Admin페이지만들기/json_data/'+data_name+'.json'
+    with open(file_path, 'w') as f:
+        json.dump(array_row, f)
+driver.quit()
